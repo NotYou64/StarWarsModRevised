fc7701d8fed876b2f204d6bc7433499c335296bb
Cole D <coledelong421@gmail.com>
2020-12-06 17:00:02 -0600

    Lightsabers now fullly functional
    Lightsabers added and able to be crafted in survival mode. Additionally added the components of the lightsaber, including kyber crystal, emitter, grip, and activation switch.

--- src/main/java/com/coledelong/star_wars_mod/init/ModItems.java
+++ src/main/java/com/coledelong/star_wars_mod/init/ModItems.java
@@ -18,7 +18,18 @@ public class ModItems {
 	public static final ToolMaterial LIGHTSABER_MATERIAL = EnumHelper.addToolMaterial("lightsaber_material", 4, Integer.MAX_VALUE, 40.0F, 100.0F, 40);
 	
 	// Items
-	public static final List<Item> ITEMS = new ArrayList<Item>();	
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item HILT = new ItemBase("hilt");
+	public static final Item HAND_GRIP = new ItemBase("hand_grip");
+	public static final Item BLADE_EMITTER = new ItemBase("blade_emitter");
+	public static final Item ACTIVATOR_SWITCH = new ItemBase("activator_switch");
+	
+	public static final Item KYBER_CRYSTAL_GREEN = new ItemBase("kyber_crystal_green");
+	public static final Item KYBER_CRYSTAL_ORIGINAL_BLUE = new ItemBase("kyber_crystal_original_blue");
+	public static final Item KYBER_CRYSTAL_YOUNGLING_BLUE = new ItemBase("kyber_crystal_youngling_blue");
+	public static final Item KYBER_CRYSTAL_PURPLE = new ItemBase("kyber_crystal_purple");
+	public static final Item KYBER_CRYSTAL_RED = new ItemBase("kyber_crystal_red");
 	
 	// Tools
 	public static final ItemSword LIGHTSABER_GREEN = new Lightsaber("lightsaber_green", LIGHTSABER_MATERIAL);
